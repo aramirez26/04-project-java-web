d48ab388f347121624dde49b1d9a3dcf34de4d3e
aramirez <tonifer627@gmail.com>
2017-09-19 22:49:09 -0400

    Merge origin/master into HEAD
    Conflicts:
    	cobertura/reports/cobertura-report/coverage.xml

--- test/Controler/mensajes2Test.java
+++ test/Controler/mensajes2Test.java
@@ -5,6 +5,10 @@
  */
 package Controler;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,48 +20,64 @@ public class mensajes2Test {
     
     public mensajes2Test() {
     }
-
-/*    @Test
-    public void testGetTipo() {
-        System.out.println("getTipo");
-        mensajes2 instance = null;
-        String expResult = "";
-        String result = instance.getTipo();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
     }
 
-    @Test
-    public void testSetTipo() {
-        System.out.println("setTipo");
-        String tipo = "";
-        mensajes2 instance = null;
-        instance.setTipo(tipo);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testGetMensaje() {
-        System.out.println("getMensaje");
-        mensajes2 instance = null;
-        String expResult = "";
-        String result = instance.getMensaje();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
-    }
-
-    @Test
-    public void testSetMensaje() {
-        System.out.println("setMensaje");
-        String mensaje = "";
-        mensajes2 instance = null;
-        instance.setMensaje(mensaje);
-        fail("The test case is a prototype.");
-    }*/
+//    @Test
+//    public void testGetTipo() {
+//        System.out.println("getTipo");
+//        mensajes2 instance = null;
+//        String expResult = "";
+//        String result = instance.getTipo();
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    @Test
+//    public void testSetTipo() {
+//        System.out.println("setTipo");
+//        String tipo = "";
+//        mensajes2 instance = null;
+//        instance.setTipo(tipo);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    @Test
+//    public void testGetMensaje() {
+//        System.out.println("getMensaje");
+//        mensajes2 instance = null;
+//        String expResult = "";
+//        String result = instance.getMensaje();
+//        assertEquals(expResult, result);
+//        fail("The test case is a prototype.");
+//    }
+//
+//    @Test
+//    public void testSetMensaje() {
+//        System.out.println("setMensaje");
+//        String mensaje = "";
+//        mensajes2 instance = null;
+//        instance.setMensaje(mensaje);
+//        fail("The test case is a prototype.");
+//    }
 
     @Test
     public void testGenerarRespuesta() {
-                System.out.println("generarRespuesta");
+        System.out.println("generarRespuesta");
         mensajes2 instance = new mensajes2("falla de carga","error");
         String ubicacion = "images/error.png";
         String tipo = instance.tipo;
